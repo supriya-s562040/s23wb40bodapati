d69a5ff3eca88b54211d7bda5721edb87688bc8f
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-04-05 22:31:45 -0500

    assignment 10

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # s23wb40bodapati
+
+class: Vehicle
+
+attributes:
+company(String)
+price(number)
+color(String)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+var vehicleRouter = require('./routes/vehicle');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
+app.use('/vehicle', vehicleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
